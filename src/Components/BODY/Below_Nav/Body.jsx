cbd65f5345de5323b8e10f0a4196c3ffd3e2c547
Jigisha336 <Jigishagachuru336@gmail.com>
2025-09-04 12:43:30 +0300

    Rename image import and update class name for consistency in Below-nav component

--- src/Components/BODY/Below_Nav/Body.jsx
+++ src/Components/BODY/Below_Nav/Body.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import image1 from '../../../Assets/image2.jpg'
+import image2 from '../../../Assets/image2.jpg'
 import './Below-nav.css'
 import author from '../../../Assets/author.jpg'
 
 function Body() {
   return (
     <div>
-      <img src={image1} alt="" className='image1'/>
+      <img src={image2} alt="" className='image2'/>
       <div className='text1'>
         <h1>Did you know?</h1>
         <h3>Coffee is one of the leading exports in kenya.It is grown in many parts of the country and farmers profit from it a lot. </h3>
