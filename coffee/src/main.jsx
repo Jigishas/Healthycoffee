a351ff3131c83e25d7d9d26c105c0e056c09058f
Jigisha336 <jigishagachuru336@gmail.com>
2025-11-26 20:25:14 +0300

    few styling commits

--- coffee/src/main.jsx
+++ coffee/src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import App from './App.jsx'
 import './serviceWorker.js'
 
+const muiTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#d97706', // amber-600
+    },
+    secondary: {
+      main: '#059669', // emerald-600
+    },
+  },
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ChakraProvider>
+      <ThemeProvider theme={muiTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </ChakraProvider>
   </StrictMode>,
 )
